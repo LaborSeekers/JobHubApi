6979f62c2daf997a9d3d90969e1309622ddf950a
KarimS21 <U201816862@upc.edu.pe>
2024-09-01 14:32:08 -0500

    Inicio del Proyecto - Api - Ingeniera de Software

---
+++ src/main/java/thelaborseekers/jobhubapi/JobHubApiApplication.java
@@ -0,0 +1,13 @@
+package thelaborseekers.jobhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobHubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thelaborseekers/jobhubapi/JobHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package thelaborseekers.jobhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
