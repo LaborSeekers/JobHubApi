fece6f91f02dac02ff8958de7af8002f4ac575b3
EtSenia <85327298+EtSenia@users.noreply.github.com>
2024-09-12 12:28:36 -0500

    fix/endpoints-name-fixed

--- src/main/java/thelaborseekers/jobhubapi/api/AdminFavoriteJobOffersController.java
+++ src/main/java/thelaborseekers/jobhubapi/api/AdminFavoriteJobOffersController.java
@@ -4,13 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import thelaborseekers.jobhubapi.model.entity.FavoriteJobOffers;
 import thelaborseekers.jobhubapi.model.entity.JobOffer;
 import thelaborseekers.jobhubapi.model.entity.Postulante;
 import thelaborseekers.jobhubapi.service.AdminFavoriteJobOffersService;
-import thelaborseekers.jobhubapi.service.AdminPostulanteService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,35 +16,27 @@ import java.util.List;
 public class AdminFavoriteJobOffersController {
     private final AdminFavoriteJobOffersService adminFavoriteJobOffersService;
 
-    @GetMapping("/fav")
-    public ResponseEntity<List<Postulante>> listPostulantes(int job_offer_id) {
-        List<FavoriteJobOffers> lista = adminFavoriteJobOffersService.get_PostulantsForJobOffer(job_offer_id);
-        List<Postulante> res = new ArrayList<>();
-        for(FavoriteJobOffers fj : lista) {
-            res.add(fj.getPostulante());
-        }
-        return ResponseEntity.ok(res);
+    @GetMapping("/job-offers/{job_offer_id}/favorited-by")
+    public ResponseEntity<List<Postulante>> listPostulantes(@PathVariable int job_offer_id) {
+        List<Postulante> lista = adminFavoriteJobOffersService.get_PostulantsForJobOffer(job_offer_id);
+        return ResponseEntity.ok(lista);
     }
 
-    @GetMapping("/fav2")
-    public ResponseEntity<List<JobOffer>> listFavoriteJobOffers(int postulant_id) {
-        List<FavoriteJobOffers> lista = adminFavoriteJobOffersService.get_JobOffers(postulant_id);
-        List<JobOffer> res = new ArrayList<>();
-        for(FavoriteJobOffers fj : lista) {
-            res.add(fj.getJobOffer());
-        }
-        return ResponseEntity.ok(res);
+    @GetMapping("/postulant/{postulant_id}/favorite-job-offers")
+    public ResponseEntity<List<JobOffer>> listFavoriteJobOffers(@PathVariable int postulant_id) {
+        List<JobOffer> lista = adminFavoriteJobOffersService.get_JobOffers(postulant_id);
+        return ResponseEntity.ok(lista);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/delete")
-    public ResponseEntity<Void> deleteFavoriteJobOffer(int job_offer_id, int postulant_id) {
+    @DeleteMapping("/postulant/{postulant_id}/remove-favorite-job-offer/{job_offer_id}")
+    public ResponseEntity<Void> deleteFavoriteJobOffer(@PathVariable int job_offer_id,@PathVariable int postulant_id) {
         adminFavoriteJobOffersService.delete(job_offer_id, postulant_id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<Void> addFavoriteJobOffer(int job_offer_id, int postulant_id) {
+    @PostMapping("/postulant/{postulant_id}/add-favorite-job-offer")
+    public ResponseEntity<Void> addFavoriteJobOffer(@RequestParam int job_offer_id, @PathVariable int postulant_id) {
         adminFavoriteJobOffersService.create(job_offer_id, postulant_id);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
