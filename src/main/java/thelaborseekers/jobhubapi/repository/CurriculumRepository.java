17b750762c3e15b1b13fac55b1e9c76e11061a8c
AnthonyConH <102244542+AnthonyConH@users.noreply.github.com>
2024-09-12 15:40:06 -0500

    US09-CreacionCV y US19-ActualizaciónCV

---
+++ src/main/java/thelaborseekers/jobhubapi/repository/CurriculumRepository.java
@@ -0,0 +1,10 @@
+package thelaborseekers.jobhubapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import thelaborseekers.jobhubapi.model.entity.Curriculum;
+import java.util.Optional;
+
+public interface CurriculumRepository extends JpaRepository<Curriculum, Integer> {
+    
+    Optional<Curriculum> findByPostulanteId(Integer postulanteId);
+}
