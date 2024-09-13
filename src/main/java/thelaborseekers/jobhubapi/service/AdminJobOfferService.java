8d25184ab1eed3338ba215a5d86f6038775b65b1
Samol19 <paredessamuel19@gmail.com>
2024-09-12 19:02:28 -0500

    Añadido Reviews en Sistema de reputacion

--- src/main/java/thelaborseekers/jobhubapi/service/AdminJobOfferService.java
+++ src/main/java/thelaborseekers/jobhubapi/service/AdminJobOfferService.java
@@ -18,4 +18,6 @@ public interface AdminJobOfferService {
     void deleteJobOffer(Integer jobOfferId);
 
     Reputation getReputationbyJobOfferId(Integer jobOfferId);
+
+    JobOffer findById(Integer jobOfferId);
 }
