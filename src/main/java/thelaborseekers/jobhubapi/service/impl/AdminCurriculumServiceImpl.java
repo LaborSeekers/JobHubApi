3e534362502873862c39443033ff7844f6209c7e
AnthonyConH <102244542+AnthonyConH@users.noreply.github.com>
2024-09-26 18:08:00 -0500

    Actualizacion y mejora_US09_US19_US11

--- src/main/java/thelaborseekers/jobhubapi/service/impl/AdminCurriculumServiceImpl.java
+++ src/main/java/thelaborseekers/jobhubapi/service/impl/AdminCurriculumServiceImpl.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import thelaborseekers.jobhubapi.model.entity.Curriculum;
+import thelaborseekers.jobhubapi.model.entity.Postulante;
 import thelaborseekers.jobhubapi.repository.CurriculumRepository;
+import thelaborseekers.jobhubapi.repository.PostulanteRepository;
 import thelaborseekers.jobhubapi.service.AdminCurriculumService;
 
 import java.util.List;
@@ -15,7 +17,8 @@ import java.util.List;
 @Service
 public class AdminCurriculumServiceImpl implements AdminCurriculumService {
     private final CurriculumRepository curriculumRepository;
-
+    private final PostulanteRepository postulanteRepository;
+    
     @Transactional(readOnly = true)
     @Override
     public List<Curriculum> findAll() {
@@ -31,7 +34,19 @@ public class AdminCurriculumServiceImpl implements AdminCurriculumService {
     @Transactional
     @Override
     public Curriculum create(Curriculum curriculum) {
-        return curriculumRepository.save(curriculum);
+        if (curriculum.getPostulante() == null || curriculum.getPostulante().getId() == null) {
+        throw new RuntimeException("Postulante ID is required");
+    }
+
+    // Obtener el postulante a partir de su ID
+    Postulante postulante = postulanteRepository.findById(curriculum.getPostulante().getId())
+            .orElseThrow(() -> new RuntimeException("Postulante not found with id: " + curriculum.getPostulante().getId()));
+
+    // Asignar el postulante al curriculum
+    curriculum.setPostulante(postulante);
+
+    // Guardar el currículum con el postulante asociado
+    return curriculumRepository.save(curriculum);
     }
 
     @Transactional(readOnly = true)
